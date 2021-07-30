0e18850d7fc74ab44504a6b023fc414f0368b5b9
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-07-30 12:24:28 -0700

    added ON DELETE statements to schema.sql, created an mysql connection

--- db/schema.sql
+++ db/schema.sql
@@ -8,14 +8,14 @@ CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
-
+ 
 DROP TABLE IF EXISTS roles;
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES departments(id)
+    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS employees;
@@ -25,6 +25,6 @@ CREATE TABLE employees (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
+    FOREIGN KEY (role_id) REFERENCES roles(id), ON DELETE CASCADE
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
 
-const mysql = require('mysql12');
+require('dotenv').config();
 
-const db = mysql.createConnection (
-
-
-);
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: process.env.DB_PASS,
+      database: 'employees_db'
+    },
+    console.log(`Connected to the books_db database.`)
+  );
