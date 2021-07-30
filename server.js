f28abc7b63e0504ce9831b355b561348275667d8
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-07-30 14:44:05 -0700

    created first prompt and starting a switch case from the answer

--- server.js
+++ server.js
@@ -19,10 +19,66 @@ const db = mysql.createConnection(
 
 const query = util.promisify(db.query).bind(db);
 
-const selectAllEmployees = async () => {
-    const results = await query("SELECT * FROM employees");
-    await console.log("async function");
-    await console.table(results);
+// how to do async delete later
+// const selectAllEmployees = async () => {
+//     const results = await query("SELECT * FROM employees");
+//     await console.log("async function");
+//     await console.table(results);
+// };
+
+
+const nextActionList = [
+    {
+        type: 'list',
+        message:'What would you like to do?',
+        name:'choice',
+        choices: [
+            "View All Employees",
+            "Add an Employee",
+            "Update Employee Role",
+            "View All Roles",
+            "Add a Role",
+            "View All Departments",
+            "Add a Department",
+            "Quit"
+        ]
+    }
+];
+
+const askNextAction = async () => {
+    const nextActionAnswer = await inquirer.prompt(nextActionList);
+
+    await console.log(nextActionAnswer.choice);
+
+    switch (nextActionAnswer.choice) {
+        case "View All Employees":
+
+            break;
+        case "Add an Employee":
+
+            break;
+        case "Update Employee Role":
+
+            break;
+        case "View All Roles":
+
+            break;
+        case "Add a Role":
+
+            break;
+        case "View All Departments":
+
+            break;
+        case "Add a Department":
+
+            break;
+        case "Quit":
+
+            break;
+    };
 };
 
-selectAllEmployees();
+
+
+// stand-in init function
+askNextAction();
